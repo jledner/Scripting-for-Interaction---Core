6ae1864cb2b60ce6b673e92b47afbc3f51235ced
jledner <jledner7@gmail.com>
2023-03-18 11:48:20 -0400

    add gate open/close and jumppads

--- Assets/Scripts/FirstPersonMovement.cs
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -8,28 +8,38 @@ public class FirstPersonMovement : MonoBehaviour
     // Player's movement parameters
     public Vector3 direction;
     public float speed;
+
+    // Player's rigidbody
+    public Rigidbody rb; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // All physics calculations happen in Fixed Update
+    void FixedUpdate()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        // make world direction into local direction
+        Vector3 localDirection = transform.TransformDirection(direction);
+
+        //move using physics
+        rb.MovePosition(rb.position + (localDirection * speed * Time.deltaTime));
+        //transform.Translate(direction * speed * Time.deltaTime);
     }
 
     // OnPlayerMove event handler
     public void OnPlayerMove(InputValue value)
     {
         // A vector with x and y componenets corresponding to the player's WASD and arrow inputs
-        // boht components are in the range [-1, 1]
+        // both components are in the range [-1, 1]
         Vector2 inputVector = value.Get<Vector2>();
-        direction = new Vector3(inputVector.x, 0, inputVector.y);
+        //direction = new Vector3(inputVector.x, 0, inputVector.y);
 
         //move in the XZ plane
         direction.x = inputVector.x;
         direction.z = inputVector.y;
+        direction.y = 0f;
     }
 }
